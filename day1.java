0a56ca41e2d3339e13a8beb39774a4f8fb581f3c
Abegail <elizanabegail1124@gmail.com>
2026-03-14 21:12:00 +0800

    basic syntax
    just a practice of the syntax

---
+++ day1.java
@@ -0,0 +1,7 @@
+// so tf ill be learning the basics once again omg
+// so ill just be doing the basic syntax cuz y tf not
+public class day1{
+public static void main(String[] args) {
+System.out.println("Hello, World");
+}
+}
