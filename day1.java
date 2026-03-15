cd26a4ccd70e868f0233037852a24a5a8216de51
Abegail <elizanabegail1124@gmail.com>
2026-03-15 21:31:11 +0800

    so i just add like lyrics cuz they said to add some

--- day1.java
+++ day1.java
@@ -2,6 +2,17 @@
 // so ill just be doing the basic syntax cuz y tf not
 public class day1{
 public static void main(String[] args) {
+    // this is like all the variations of sop
 System.out.println("Hello, World");
+System.out.print("Boogie\n");
+    System.out.print("I am gay asf bro");
+    System.out.print("\n\n");
+
+    // ima type this shit as like lyrics cuz they said y not
+    System.out.println("==========================================");
+System.out.println("I'm freezing outside, I feel my skin tight");
+System.out.print("My coat is inside, but I look up at you\n");
+System.out.println("I tracked your plane ride for when you're in tonight");
+System.out.print("Tell me, when is the next time I'll run into you? (Oh)\n");
 }
 }
