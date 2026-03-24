ee54f7c103f65d9c9a3d337bc0ab3184b89ef041
Abegail <elizanabegail1124@gmail.com>
2026-03-24 20:33:07 +0800

    i just did variables

---
+++ day2.java
@@ -0,0 +1,22 @@
+// fucke yea variables omfg
+public class day2 {
+    public static void main(String[] args){
+
+        // okey so i have to fucking do primitative type shit datatypes
+        String name = "Abby"; // ts is a reference type of data type
+        int age = 19; // now ts is a primitive fucking data type
+        double money = 100.00; // ts is a decimal i can also use float
+        char grade = 'B'; // man ts is just a single character like damn
+        boolean isPassing = false; // ts is just sad man imagine failing in life
+
+        System.out.println("This person's name is " + name + " she's " + age + " years old which is kinda old ig");
+        System.out.println("her current finance is " + money + " and it's so sad to hear and she also is kinda failing with a " + grade + " this semester");
+
+        if (isPassing){
+            System.out.println("Congrats girl ur passinnggggg");
+        }
+        else {
+            System.out.println("u r a failure to ur ancestors");
+        }
+    }
+}
